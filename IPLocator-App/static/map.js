1d398570fb5ce57148c03820ca7f138673d2d86d
karie <25291562+kariemoorman@users.noreply.github.com>
2024-04-06 20:30:00 -0700

    Update map.js

--- IPLocator-App/static/map.js
+++ IPLocator-App/static/map.js
@@ -19,6 +19,17 @@ document.addEventListener('DOMContentLoaded', function () {
         })
     }).addTo(map);
 
+    var marker2 = L.marker([lat, long], {
+        icon: L.icon({
+            iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+            shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+            iconSize: [20, 31],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowSize: [41, 41]
+        })
+    }).addTo(map);
+
     var panorama = L.panorama().addTo(map);
     panorama.setPosition([latitude, longitude]);
     map.on('click', function(e) {
