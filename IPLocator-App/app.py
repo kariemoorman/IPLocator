c481b3a293c34adc622974458d5ef95e20f86822
karie <25291562+kariemoorman@users.noreply.github.com>
2024-04-07 20:30:43 -0700

    Update app.py

--- IPLocator-App/app.py
+++ IPLocator-App/app.py
@@ -17,23 +17,23 @@ def index():
     msg = None
     locator = None
     if request.method == 'POST':
-        logging.info("Received POST request.")
+        logging.info(f"Received POST request from IP address: {request.remote_addr}.")
         ip_or_url = request.form['ip_or_url'].strip()
-        logging.info(f"Received input: {ip_or_url}.")
+        logging.info(f"Classifying input value: {ip_or_url} from {request.remote_addr}.")
         ipv4_pattern = r'^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
         if re.match(ipv4_pattern, ip_or_url):
-            logging.info("Input matches IPv4 pattern.")
+            logging.info(f"Received valid IPv4 address: {ip_or_url} from {request.remote_addr}.")
             ip = str(ip_or_url)
             locator = IPLocator(ip_address=ip)
         else:
             try:
-                logging.info("Input does not match IPv4 pattern, attempting DNS resolution.")
+                logging.info(f"Input does not match IPv4 pattern, attempting DNS resolution for: {ip_or_url} from {request.remote_addr}.")
                 socket.gethostbyname(ip_or_url)
-                logging.info("Input matches DNS pattern.")
+                logging.info(f"Input matches DNS pattern for: {ip_or_url} from {request.remote_addr}.")
                 url = str(ip_or_url)
                 locator = IPLocator(url=url)
             except socket.gaierror:
-                logging.error("Invalid IP address or domain name provided.")
+                logging.error(f"Invalid IP address or domain name provided for: {ip_or_url} from {request.remote_addr}.")
                 msg = "🚨 Please Enter a Valid IP Address or Domain Name. 🚨"
                 ip_info, lat, long, latitude, longitude = None, None, None, None, None
                 return render_template('index.html', ip_info=ip_info, lat=lat, long=long, latitude=latitude, longitude=longitude, msg=msg)
