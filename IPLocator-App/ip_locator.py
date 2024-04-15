df3c603d3f903fcf6ceecb4025c9a54358199153
karie <25291562+kariemoorman@users.noreply.github.com>
2024-04-14 21:47:38 -0700

    Update ip_locator.py

--- IPLocator-App/ip_locator.py
+++ IPLocator-App/ip_locator.py
@@ -60,29 +60,30 @@ class IPLocator:
         try:
             response = requests.get(f"http://ip-api.com/json/{self.ip_address}?fields=58454015")
             ip_data = response.json()
+            address = self.get_address(ip_data.get("lat"), ip_data.get("lon"))
+            output = {
+                "ip": self.ip_address,
+                'ip_domain': self.url,
+                "hostname": hostname,
+                "isp": ip_data.get("isp"),
+                "org": ip_data.get("org", None),
+                "as": ip_data.get("as"),
+                #"continent_code": ip_data.get("continentCode"),
+                "dc_region": ip_data.get("regionName"),
+                #"district": ip_data.get("district"),
+                #"dc_postal_code": ip_data.get("zip"),
+                "dc_timezone": ip_data.get("timezone"),
+                "dc_latitude": ip_data.get("lat"),
+                "dc_longitude": ip_data.get("lon"),
+                "dc_address": address,
+                #"proxy": ip_data.get("proxy"),
+                "is_host": ip_data.get("hosting"),
+                #"mobile": ip_data.get("mobile"),
+                "is_private" : self.is_private_ip()
+            }
         except requests.RequestException:
             ip_data = {}
-        address = self.get_address(ip_data.get("lat"), ip_data.get("lon"))    
-        output = {
-            "ip": self.ip_address,
-             "hostname": hostname,
-             'domain_name': self.url,
-             "isp": ip_data.get("isp"),
-             "org": ip_data.get("org"),
-             "asin": ip_data.get("as"),
-             "proxy": ip_data.get("proxy"),
-             "hosting": ip_data.get("hosting"),
-             "mobile": ip_data.get("mobile"),
-             "is_private": self.is_private_ip(),
-             "continent_code": ip_data.get("continentCode"),
-             "region": ip_data.get("regionName"),
-             "district": ip_data.get("district"),
-             "zip_code": ip_data.get("zip"),
-             "time_zone": ip_data.get("timezone"),
-             "lat": ip_data.get("lat"),
-             "long": ip_data.get("lon"),
-             "address": address
-        }
+
         return output
     
     def get_location(self):
@@ -109,27 +110,75 @@ class IPLocator:
                 output = {
                     # 'ip_address': self.ip_address,
                     # 'url': self.url,
-                    'country_code': country_code,
-                    'continent_code': continent_code,
-                    'country': country,
-                    'timezone': timezone,
-                    'state': state,
-                    'region_code': region_code,
-                    'city': city,
-                    'area_code': area_code,
-                    'postal_code': postal_code,
-                    'latitude': latitude,
-                    'longitude': longitude,
-                    'physical_address': address
+                    #'country_code': country_code,
+                    #'continent_code': continent_code,
+                    #'country': country,
+                    'az_timezone': timezone,
+                    # 'state': state,
+                    #'region_code': region_code,
+                    # 'city': city,
+                    # 'area_code': area_code,
+                    # 'az_postal_code': postal_code,
+                    'az_latitude': latitude,
+                    'az_longitude': longitude,
+                    'az_address': address
                     }
                 return output
             except geoip2.errors.AddressNotFoundError:
                 return None
+
+    def get_company_data(self): 
+        try:
+            response = requests.get(f"https://api.ipapi.is?q={self.ip_address}")
+            company_data = response.json()
             
+            company_info = company_data['company']
+            asn = company_data['asn']
+            location = company_data['location']
+
+            address = self.get_address(location.get("latitude"), location.get("longitude"))
+            
+            output = {
+                "is_bogon": company_data.get("is_bogon"),
+                "is_mobile": company_data.get("is_mobile"),
+                "is_crawler": company_data.get("is_crawler"),
+                "is_datacenter": company_data.get("is_datacenter"),
+                "is_tor": company_data.get("is_tor"),
+                "is_proxy": company_data.get("is_proxy"),
+                "is_vpn": company_data.get("is_vpn"),
+                'is_type': company_info.get('type'),
+                'company_org': company_info.get('name'),
+                'asn_org': asn.get('org'),
+                'company_domain': company_info.get('domain', None),
+                'asn_domain': asn.get('domain'),
+                'network': company_info.get('network'),
+                'route': asn.get('route'),
+                'active_status': asn.get('active'),
+                'created_date': asn.get('created', None),
+                'updated_date': asn.get('updated', None),
+                "is_abuser": company_data.get("is_abuser"),
+                'company_abuser_score': company_info.get('abuser_score'),
+                'asn_abuser_score': asn.get('abuser_score'),
+                'company_latitude':  location.get('latitude'),
+                'company_longitude': location.get('longitude'),
+                'company_timezone': location.get('timezone'),
+                'company_address': address
+            }
+        except requests.RequestException:
+            output = {}
+        
+        return output 
+    
     def get_ip_info(self):
         location_data = self.get_location()
+        company_data = self.get_company_data()
         type_data = self.get_ip_type()
         self.set_filepath()
+        
+        if company_data: 
+            for key, value in company_data.items():
+                type_data[key] = value
+                
         if location_data: 
             for key, value in location_data.items():
                 type_data[key] = value
